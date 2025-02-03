fd481cadca85bd8c4507f4dfaddf8caa97a2ab44
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2025-02-03 20:41:55 +0800

    to stop at context

---
+++ crowdfunding/programs/crowdfunding/src/lib.rs
@@ -0,0 +1,26 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7bv9udZUtx7nmrMYUz9VwWTWrVsbXpSDq4BApdAyTbiu");
+
+#[program]
+pub mod crowdfunding {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space = 9000, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
